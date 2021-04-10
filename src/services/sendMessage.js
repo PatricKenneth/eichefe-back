4609cf7f4d55521a6d5ac457ef68ac0dff88a365
Patric Kenneth <patrickenneth77@gmail.com>
2021-04-10 12:08:57 -0300

    Corringindo bug do bot do telegram

--- src/services/sendMessage.js
+++ src/services/sendMessage.js
@@ -1,4 +1,13 @@
-exports.messageSend = async (data, bot) => {
+const TelegramBot = require(`node-telegram-bot-api`);
+
+// const TOKEN = '1652801314:AAE_XRZdfDBa0r4hp2kiyAnZi_puP_r-F8g';
+// const CHAT_ID = 1094771835
+const TOKEN = '1629311094:AAH7Ml3KPT4xRiZ17lzGzodVEQpiYte2n7c';
+const CHAT_ID = 1609702003;
+
+exports.messageSend = async (data) => {
+    const bot = new TelegramBot(TOKEN);
+    
     const response = await bot.sendMessage(CHAT_ID, `
     Data: ${data.requestDate}
     Nome: ${data.name}
@@ -6,6 +15,7 @@ exports.messageSend = async (data, bot) => {
     E-mail: ${data.email}
     Descrição: ${data.description}
     `);
+
     return response;
 };
 
